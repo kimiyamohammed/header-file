f1dcdd528639008ac83e4239fda14dc0b073e858
kimiyamohammed <37698128+kimiyamohammed@users.noreply.github.com>
2018-05-31 16:03:57 -0700

    Add files via upload

---
+++ header file/main.cpp
@@ -0,0 +1,7 @@
+#include "se.h"
+#include <iostream>
+using namespace std;
+int main(){
+cout << "The square of 3 is " << square(3);
+return 0;
+}
---
+++ header file/se.h
@@ -0,0 +1,4 @@
+#ifndef SE_H
+#define SE_H
+int square(int n);
+#endif
